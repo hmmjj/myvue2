48c15c98d5600d2ce990af24fd3877b5c6573483
hmm <2267448785@qq.com>
2020-07-05 16:38:07 +0800

    安装less并添加home文件

---
+++ src/page/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home_container">
+        萌萌哒
+    </div>
+</template>
+<script>
+export default {
+    name: 'home'
+}
+</script>
+<style lang="less" scoped>
+
+</style>
+
+
