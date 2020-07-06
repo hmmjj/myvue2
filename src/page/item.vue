00a0778dc62a388598e5cd37e43d7cc4a86b732f
hmm <2267448785@qq.com>
2020-07-06 17:07:34 +0800

    添加item文件并完成item页面布局

---
+++ src/page/item.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="item_container">
+        <common father-component="item"></common>
+    </div>
+</template>
+<script>
+import Common from '@/components/common'
+export default {
+    name: 'item',
+    components: { Common }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
+
+
