031f9e3f335e8bbd4e471a1f4f43127707a0bf69
hmm <2267448785@qq.com>
2020-07-06 19:27:44 +0800

    安装vuex并在state状态中添加数据

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
+import store from './store/store'
 import './config/rem'
 import '!style-loader!css-loader!less-loader!./assets/style/common.less'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
