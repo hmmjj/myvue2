da77114eb8cc430a6c94562bed8d475280648fee
hmm <2267448785@qq.com>
2020-07-06 19:31:03 +0800

    。

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
 
-const store = new Vue.Store({
+const store = new Vuex.Store({
     state: {
         level: '第一周', //活动周数
         itemNum: 1, // 第几题
